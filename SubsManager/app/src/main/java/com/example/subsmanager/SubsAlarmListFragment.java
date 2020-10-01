46fd921d754df546ff5654f0b1a941f0affe81b1
jswfs1284 <jswfs1284@naver.com>
2020-10-02 03:26:47 +0900

    승원언니^^ - FireBase 설정 파일, Fragment 레이어 연동 오류난거 '살짝' 수정

--- SubsManager/app/src/main/java/com/example/subsmanager/SubsAlarmListFragment.java
+++ SubsManager/app/src/main/java/com/example/subsmanager/SubsAlarmListFragment.java
@@ -23,6 +23,6 @@ public class SubsAlarmListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subs_alarm_list2, container, false);
+        return inflater.inflate(R.layout.fragment_subs_alarm_list, container, false);
     }
 }
--- SubsManager/app/src/main/java/com/example/subsmanager/SubsListFragment.java
+++ SubsManager/app/src/main/java/com/example/subsmanager/SubsListFragment.java
@@ -23,6 +23,6 @@ public class SubsListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subs_list3, container, false);
+        return inflater.inflate(R.layout.fragment_subs_list, container, false);
     }
 }

8cd5525f40fd17f71aa46fcd76bc97c4a61899be
dndbekfrl1 <dndbekfrl1@gmail.com>
2020-10-03 19:14:42 +0900

    탭 메뉴 item 추가

--- SubsManager/app/src/main/java/com/example/subsmanager/MainActivity.java
+++ SubsManager/app/src/main/java/com/example/subsmanager/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.subsmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView tabMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabMenu = findViewById(R.id.tab_view);
+        tabMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() { //NavigationItemSelecte
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                BottomNavigate(menuItem.getItemId());
+                return true;
+            }
+        });
+    }
+
+    private void BottomNavigate(int id) {
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.subs_list) {
+                fragment = new SubsListFragment();
+            } else if (id == R.id.board_list){
+                fragment = new BoardListFragment();
+            } else if(id==R.id.subs_recommend) {
+                fragment = new SubsRecommendFragment();
+            }
+            else{
+                fragment = new MypageFragment();
+            }
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
     }
 }
